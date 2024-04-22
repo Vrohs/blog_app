7831044851951696ef05b2a0ed20fe4412513085
Vrohs <info.rohsgaming536@gmail.com>
2024-04-22 22:02:56 +0530

    Update MongoDB connection and add user schema and model in index.js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+
+// mongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/lecture01')
+.then(() => console.log('connected to mongodb://127.0.0.1:27017/lecture01'))
+.catch((err) => {
+    console.error(err)
+})
+
+
+// mongoDB schema
+const userSchema = new mongoose.Schema({
+
+    firstName:{
+        
+        type: String,
+        required: true
+    },
+
+    email:{
+
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    gender:{
+
+        type: String,
+        enum: ['Male', 'Female', 'Other']
+    }
+});
+
+const userModel = mongoose.model("user", userSchema);
+
 
 app.use((req,res,next) => {
     next()
@@ -8,7 +44,7 @@ app.use((req,res,next) => {
 app.use(express.static('.'))
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true               }))
+app.use(express.urlencoded({extended:true}))
 
 app.set("view engine", "ejs")
 
